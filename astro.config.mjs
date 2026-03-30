09432ceaebb7de997d501dde44e8c680738dd4e9
martirale <hola@alemartir.com>
2026-03-30 02:11:16 -0600

    Add init Astro files and customization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,14 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import rehypeExternalLinks from "rehype-external-links";
 
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  markdown: {
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: "_blank", rel: ["noopener"] }],
+    ],
+  },
+});
